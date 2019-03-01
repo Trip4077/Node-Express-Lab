64ab30bd04fcec136826cee2c96e9d6969076c41
Trip1701 <bj2336@email.vccs.edu>
2019-03-01 17:54:20 -0500

    updatePost and deletePost created

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const PORT = '5000';
 
 server.use(express.json());
 
+//CREATE
 server.post('/api/posts', async (req,res) => {
     try {
         const post = req.body;
 
         if(post.title && post.contents) {
             const posted = await db.insert(post);
-            res.json(posted);
+            const newPost = await db.findById(posted.id)
+            res.json(newPost[0]);
         } else {
             res.status(500).json({ message: 'Invalid Data'})
         }
@@ -23,6 +25,7 @@ server.post('/api/posts', async (req,res) => {
     }
 }) 
 
+//READ
 server.get('/api/posts', async (req,res) => {
     try {
         const posts = await db.find();
@@ -39,6 +42,44 @@ server.get('/api/posts/:id', async (req,res) => {
         
         if(post) {
             res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: 'Post not found'})
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error getting post'});
+    }
+})
+
+//UPDATE
+server.put('/api/posts/:id', async (req,res) => {
+    try {
+        const post = req.body;
+        const id = req.params.id; 
+        
+        const update = await db.update(id, post); 
+        const updatedPost = await db.findById(id) 
+        if(update) {
+            res.status(200).json(updatedPost[0])
+        } else {
+            res.status(404).json({ message: 'Post not found'})
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error getting post'});
+    }
+})
+
+//DELETE
+server.delete('/api/posts/:id', async (req,res) => {
+    try {
+        const id = req.params.id; 
+        
+        const deletedPost = await db.findById(id) 
+        const deleted = await db.remove(id); 
+        
+        if(deleted) {
+            res.status(200).json(deletedPost[0])
         } else {
             res.status(404).json({ message: 'Post not found'})
         }
